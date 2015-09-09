6145171e1992a385646e2d587ce2ce34885ad174
Radu <radu.bordei89@gmail.com>
2015-09-09 10:17:27 +0300

    improve other drivers.

--- src/Drivers/Yaml.php
+++ src/Drivers/Yaml.php
@@ -1,3 +1,65 @@
 <?php
 
-namespace Laravel\Settings;
+namespace Laravel\Settings\Drivers;
+
+use Laravel\Settings\Driver;
+use Laravel\Settings\DriverAble;
+
+class Yaml extends Driver implements DriverAble {
+
+    /**
+     * Retrieve all options/values.
+     *
+     * @return array
+     */
+    public function all() {
+        // TODO: Implement all() method.
+    }
+
+    /**
+     * Retrieve option .
+     *
+     * @param $key
+     * @param null $default
+     * @return mixed
+     */
+    public function get($key, $default = null) {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * update option value.
+     *
+     * @return void
+     */
+    public function update($key, $value) {
+        // TODO: Implement update() method.
+    }
+
+    /**
+     * Insert a new option and set value.
+     *
+     * @return void
+     */
+    public function inert($key, $value) {
+        // TODO: Implement inert() method.
+    }
+
+    /**
+     * delete option.
+     *
+     * @return void
+     */
+    public function delete($key) {
+        // TODO: Implement delete() method.
+    }
+
+    /**
+     * delete all options entries from .
+     *
+     * @return array
+     */
+    public function clear() {
+        // TODO: Implement clear() method.
+    }
+}
